ef042fb706e3c74130b00765c9c34442377e5179
Alex Nguyen <alexvn.edu@gmail.com>
2020-12-03 15:46:01 -0500

    refractor

--- snake-game/main_deprecated.py
+++ snake-game/main_deprecated.py
@@ -2,7 +2,7 @@
 
 """
 
-    Second main file with 8 states
+    Original (deprecated file) with 4 states
 
 """
 
@@ -23,11 +23,10 @@ state = env.reset()
 crash = False
 position = None
 d = {'UP': 0, 'DOWN': 1, 'LEFT': 2, 'RIGHT': 3}
-st = {'UP': 0, 'UP_RIGHT': 1, 'RIGHT': 2, 'DOWN_RIGHT': 3, 'DOWN': 4, 'DOWN_LEFT': 5, 'LEFT': 6, 'UP_LEFT': 7}
 db = {0: 'UP', 1: 'DOWN', 2: 'LEFT', 3: 'RIGHT'}
 dtoa = {0: 'GOING FORWARD', 1: 'STEER LEFT', 2: 'STEER RIGHT'}
 
-policies = np.asarray([[0.0] * 4] * 8)
+policies = np.asarray([[0.0] * 4] * 4)
 
 learning_rate = 0.1
 discount_rate = 0.99
@@ -39,31 +38,18 @@ max_steps_per_episode = 10000
 def get_state(head_x, head_y, food_x, food_y):
     # Food is farther or smaller in vertical dimension than 
     #   horizontal dimension
-    dif_x = abs (head_x - food_x)
-    dif_y = abs(head_y - food_y)
-    # On the same row, either right or left
-    if dif_x == 0 and dif_y != 0:
-        if food_y > head_y:
-            return st['RIGHT']
-        elif food_y < head_y:
-            return st['LEFT']
-    # Vertical dimension
-    elif dif_x != 0 and dif_y == 0:
-        if food_x > head_x:
-            return st['DOWN']
+    dif = abs (head_x - food_x) - abs(head_y - food_y)
+    # if food is farther or smaller in vertical dimension then priotitizing state x
+    if dif >= 0:
+        if food_x >= head_x:
+            return d['RIGHT']
         elif food_x < head_x:
-            return st['UP']
-        pass
-    # On diagonal
-    elif dif_x != 0 and dif_y != 0:
-        if food_x > head_x and food_y > head_y:
-            return st['DOWN_RIGHT']
-        elif food_x > head_x and food_y < head_y:
-            return st['DOWN_LEFT']
-        elif food_x < head_x and food_y > head_y:
-            return st['UP_RIGHT']
-        elif food_x < head_x and food_y < head_y:
-            return st['UP_LEFT']
+            return d['LEFT']
+    else:
+        if food_y >= head_y:
+            return d['DOWN']
+        elif food_y < head_y:
+            return d['UP']
     return -1
 
 def check_valid_move_direction(direction, head_x, head_y, prev_x, prev_y) -> bool:
@@ -113,7 +99,7 @@ def get_action(position, direction, head_x, head_y, prev_x, prev_y):
             return 0
         if direction == d['LEFT']:
             # return -1
-            print("invalid behavior!")
+            print("fucked up!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
             return random.randint(1, 2)
 
     if head_x == prev_x:
@@ -127,7 +113,7 @@ def get_action(position, direction, head_x, head_y, prev_x, prev_y):
                 return 0
             if direction == d['LEFT']:
                 # return -1
-                print("invalid behavior!")
+                print("fucked up!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                 return random.randint(1, 2)
         # Going left
         elif head_y < prev_y:
@@ -137,7 +123,7 @@ def get_action(position, direction, head_x, head_y, prev_x, prev_y):
                 return 1
             if direction == d['RIGHT']:
                 # return -1
-                print("invalid behavior!")
+                print("fucked up!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                 return random.randint(1, 2)
             if direction == d['LEFT']:
                 return 0
@@ -146,7 +132,7 @@ def get_action(position, direction, head_x, head_y, prev_x, prev_y):
         if head_x > prev_x:
             if direction == d['UP']:
                 # return -1
-                print("invalid behavior!")
+                print("fucked up!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                 return random.randint(1, 2)
             if direction == d['DOWN']:
                 return 0
@@ -160,7 +146,7 @@ def get_action(position, direction, head_x, head_y, prev_x, prev_y):
                 return 0
             if direction == d['DOWN']:
                 # return -1
-                print("invalid behavior!")
+                print("fucked up!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
                 return random.randint(1, 2)
             if direction == d['RIGHT']:
                 return 2
@@ -238,7 +224,7 @@ for episode in range(num_episodes):
         print("ACTION: " + dtoa[action])
         # print("Direction of action: " + str(db[direction]))
         print("Reward of the current state: " + str(reward))
-        print("Index: " + str(st))
+        print("Index: " + str(db))
         print("Current Q-table: \n" + str(policies))
         env.render()
 
@@ -251,3 +237,110 @@ for episode in range(num_episodes):
         
         time.sleep(0.05)
     time.sleep(0.1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
+
+print(policies)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# %%
+
+
+################### DEPRECATED ###################
+
+
+
+# PLAY
+GAME = 100
+MAX_STEP_GAME = 10000
+for episode in range(GAME):
+    position, x_food, y_food, crash = env.reset()
+    head = position[-1]
+    prev_head_x = None
+    prev_head_y = None
+
+    state = get_state(head[0], head[1], x_food, y_food)
+    for i in range(MAX_STEP_GAME):
+        
+
+        clear_output(wait=True)
+        # action = random.randint(0, 2)
+
+        # Exploration-exploitation trade-off
+        exploration_rate_threshold = random.uniform(0, 1)
+        if exploration_rate_threshold > exploration_rate:
+            # direction = int(np.argmax(policies[state,:]))
+            direction = get_next_direction(policies, position)
+            head = position[-1]
+            action = get_action(position, direction, head[0], head[1],
+                            position[-2][0] if len(position) > 1 else None,
+                            position[-2][1] if len(position) > 1 else None)
+        else:
+            direction = random.randint(0, 2)
+            action = direction
+
+        # action_str = input()
+
+        # if action_str == 'w':
+        #     direction = d['UP']
+        # elif action_str == 'a':
+        #     direction = d['LEFT']
+        # elif action_str == 'd':
+        #     direction = d['RIGHT']
+        # elif action_str == 's':
+        #     direction = d['DOWN']
+        # action = get_action(position, direction, head[0], head[1], prev_head_x, prev_head_y)
+
+        prev_head_x = head[0]
+        prev_head_y = head[1]
+        position, new_x_food, new_y_food, crash = env.step(action)
+        head = position[-1]
+        eaten: bool = head[0] == x_food and head[1] == y_food
+        reward = heuristic1(position, new_x_food, new_y_food, crash, eaten)
+        
+        new_state = get_state(head[0], head[1], new_x_food, new_y_food)
+
+        # Update Q-table
+        policies[state, direction] = policies[state, direction] * (1 - learning_rate) + \
+        learning_rate * (reward + discount_rate * np.max(policies[new_state, :]))
+        
+        state = new_state
+        x_food = new_x_food
+        y_food = new_y_food
+
+        if crash:
+            env.reset()
+            crash = False
+            break
+        print("Current position: " + str(env.x) + " " +str(env.y))
+        print("ACTION: " + dtoa[action])
+        print("Direction of action: " + str(db[direction]))
+        env.render()
+        time.sleep(0.05)
