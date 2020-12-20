f8a6d73833896c25faad24146cb612008e82f268
Alex Nguyen <alexvn.edu@gmail.com>
2020-12-20 00:26:47 -0500

    more on attetntion

---
+++ AI-Idea/attention_nn/attention_notebook.py
@@ -0,0 +1,220 @@
+# %%
+
+
+"""
+This is implemented based on these article: 
+    Read this first (every thing about attention):
+    https://github.com/tensorflow/nmt#background-on-the-attention-mechanism
+and
+    what is attention:
+    https://www.analyticsvidhya.com/blog/2019/11/comprehensive-guide-attention-mechanism-deep-learning/
+and
+    attention implmentation tutorial:
+    https://www.tensorflow.org/tutorials/text/nmt_with_attention#next_steps
+and
+    Can we apply 3D data to Dense?
+    https://stackoverflow.com/questions/54805345/can-i-use-a-3d-input-on-a-keras-dense-layer
+    
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+from pathlib import Path
+
+from sklearn.model_selection import train_test_split
+
+import unicodedata
+import re
+import io
+import time
+
+import tensorflow as tf
+
+# CONFIG
+
+
+# %%
+
+# Download the file
+path_to_zip = tf.keras.utils.get_file(
+    'spa-eng.zip', origin='http://storage.googleapis.com/download.tensorflow.org/data/spa-eng.zip',
+    extract=True)
+
+path_to_file = os.path.dirname(path_to_zip)+"/spa-eng/spa.txt"
+
+
+# %%
+
+# Converts the unicode file to ascii
+def unicode_to_ascii(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn')
+
+
+def preprocess_sentence(w):
+    w = unicode_to_ascii(w.lower().strip())
+
+    # creating a space between a word and the punctuation following it
+    # eg: "he is a boy." => "he is a boy ."
+    # Reference:- https://stackoverflow.com/questions/3645931/python-padding-punctuation-with-white-spaces-keeping-punctuation
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)
+
+    # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+    w = re.sub(r"[^a-zA-Z?.!,¿]+", " ", w)
+
+    w = w.strip()
+
+    # adding a start and an end token to the sentence
+    # so that the model know when to start and stop predicting.
+    w = '<start> ' + w + ' <end>'
+    return w
+
+en_sentence = u"May I borrow this book?"
+sp_sentence = u"¿Puedo tomar prestado este libro?"
+print(preprocess_sentence(en_sentence))
+print(preprocess_sentence(sp_sentence).encode('utf-8'))
+
+# %%
+
+# 1. Remove the accents
+# 2. Clean the sentences
+# 3. Return word pairs in the format: [ENGLISH, SPANISH]
+
+def create_dataset(path, num_examples):
+    lines = io.open(path, encoding='UTF-8').read().strip().split('\n')
+
+    word_pairs = [[preprocess_sentence(w) for w in l.split('\t')]  for l in lines[:num_examples]]
+
+    return zip(*word_pairs)
+
+en, sp = create_dataset(path_to_file, None)
+print(en[-1])
+print(sp[-1])
+
+# %%
+
+def tokenize(lang):
+    lang_tokenizer = tf.keras.preprocessing.text.Tokenizer(
+        filters='')
+    lang_tokenizer.fit_on_texts(lang)
+
+    tensor = lang_tokenizer.texts_to_sequences(lang)
+
+    tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor,
+                                                        padding='post')
+
+    return tensor, lang_tokenizer
+
+def load_dataset(path, num_examples=None):
+    # creating cleaned input, output pairs
+    targ_lang, inp_lang = create_dataset(path, num_examples)
+
+    input_tensor, inp_lang_tokenizer = tokenize(inp_lang)
+    target_tensor, targ_lang_tokenizer = tokenize(targ_lang)
+
+    return input_tensor, target_tensor, inp_lang_tokenizer, targ_lang_tokenizer
+
+# %%
+num_examples = 30000
+input_tensor, target_tensor, inp_lang, targ_lang = load_dataset(path_to_file)
+
+# Calculate max_length of the target tensors
+max_length_targ, max_length_inp = target_tensor.shape[1], input_tensor.shape[1]
+
+# Creating training and validation sets using an 80-20 split
+input_tensor_train, input_tensor_val, target_tensor_train, target_tensor_val = train_test_split(input_tensor, target_tensor, test_size=0.2)
+
+# Show length
+print(len(input_tensor_train), len(target_tensor_train), len(input_tensor_val), len(target_tensor_val))
+
+# %%
+
+BUFFER_SIZE = len(input_tensor_train)
+BATCH_SIZE = 64
+steps_per_epoch = len(input_tensor_train)//BATCH_SIZE
+embedding_dim = 256
+units = 1024
+vocab_inp_size = len(inp_lang.word_index)+1
+vocab_tar_size = len(targ_lang.word_index)+1
+
+dataset = tf.data.Dataset.from_tensor_slices((input_tensor_train, target_tensor_train)).shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
+
+# %%
+
+example_input_batch, example_target_batch = next(iter(dataset))
+example_input_batch.shape, example_target_batch.shape
+
+# %%
+
+class Encoder(tf.keras.Model):
+    def __init__(self, vocab_size, embedding_dim, enc_units, batch_sz):
+        super().__init__()
+        self.batch_sz = batch_sz
+        self.enc_units = enc_units
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(self.enc_units,
+                                        return_sequences=True,
+                                        return_state=True,
+                                        recurrent_initializer='glorot_uniform')
+    
+    def call(self, x, hidden):
+        x = self.embedding(x)
+        output, hidden = self.gru(x, initial_state=hidden)
+        return output, hidden
+
+    def initialize_hidden_state(self):
+        return tf.zeros((self.batch_sz, self.enc_units))
+
+
+# %%
+
+encoder = Encoder(vocab_inp_size, embedding_dim, units, BATCH_SIZE)
+sample_hidden = encoder.initialize_hidden_state()
+sample_output, sample_hidden = encoder(example_input_batch, sample_hidden)
+print ('Encoder output shape: (batch size, sequence length, units) {}'.format(sample_output.shape))
+print ('Encoder Hidden state shape: (batch size, units) {}'.format(sample_hidden.shape))
+
+
+# %%
+
+class Attention(tf.keras.layers.Layer):
+    def __init__(self, units):
+        super().__init__()
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, query, values):
+
+        query_with_time_axis = tf.expand_dims(query, axis=1)
+        score = self.V(tf.nn.tanh(self.W1(query_with_time_axis) + self.W2(values)))
+        
+        attention_weights = tf.nn.softmax(score, axis=1)
+
+        context_vector = attention_weights * values
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+
+        return context_vector, attention_weights
+
+
+# %%
+
+
+attention_layer = Attention(10)
+attention_result, attention_weights = attention_layer(sample_hidden, sample_output)
+
+print("Attention result shape: (batch size, units) {}".format(attention_result.shape))
+print("Attention weights shape: (batch_size, sequence_length, 1) {}".format(attention_weights.shape))
+
+# %%
+
+class Decoder (tf.keras.layers.Layer):
+    def __init__(self, vocab_size, embedding_dims, dec_units, batch_sz):
+        super().__init__()
+        self.batch_sz = batch_sz
+        
+
